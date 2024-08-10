d00006413ae2802b7534bb73c179503d54889fdc
Tom Schepers <tomschepers2007@icloud.com>
2024-08-10 16:51:26 +0200

    Added CardStack struct in rust backend, with the ability to remove a card from the list when clicked on the stack

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,60 @@
 use std::time::Duration;
 use slint::Timer;
+use std::iter::repeat;
+
+const ANIMATION_MS: u64 = 1000;
+const CARD_TYPES: u8 = 32;
+const CARD_COPIES: usize = 1;
 
 slint::include_modules!();
 
-const ANIMATION_MS: u64 = 1000;
+struct CardStack {
+	cards: Vec<u8>,
+	count: usize
+}
+
+impl CardStack {
+	fn new() -> Self {
+		let init: Vec<u8> = (0u8..CARD_TYPES)
+		.flat_map(|n| repeat(n).take(CARD_COPIES))
+		.collect();
+
+		Self {
+			cards: init,
+			count: CARD_COPIES * (CARD_TYPES as usize)
+		}
+	}
+
+
+	fn draw(&mut self, rng: &mut impl rand::Rng) -> u8 {
+		let index = rng.gen_range(0..self.count);
+		self.count -= 1;
+		self.cards.remove(index)
+	}
+}
+
+fn draw_card(window: Root, cards: &mut CardStack, rng: &mut impl rand::Rng) {
+	println!("Drew card {}", cards.draw(rng));
+	window.set_show_animation(true);
+	window.set_animation_on_target(true);
+
+	let duration = Duration::from_millis(ANIMATION_MS);
+	Timer::single_shot(duration, move || {
+		window.set_show_animation(false);
+		window.set_animation_on_target(false);
+	});
+}
 
 fn main() {
+	let mut cards = CardStack::new();
+	let mut rng = rand::thread_rng();
+
 	let window = Root::new().unwrap();
 	window.set_animation_len(ANIMATION_MS as i64);
 
 	let window_weak = window.as_weak();
-	let sleep_duration = Duration::from_millis(ANIMATION_MS);
 	window.on_stack_clicked(move || {
-		let interface = window_weak.unwrap();
-		interface.set_show_animation(true);
-		interface.set_animation_on_target(true);
-
-		Timer::single_shot(sleep_duration, move || {
-			interface.set_show_animation(false);
-			interface.set_animation_on_target(false);
-		});
+		draw_card(window_weak.unwrap(), &mut cards, &mut rng);
 	});
 
 	window.run().unwrap();
